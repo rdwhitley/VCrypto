e4c7f4a2c0930d012051140d47f9bf954add8784
Rafiq Whitley <Raw96@icloud.com>
2018-12-06 23:18:08 -0600

    Table labels Done

---
+++ src/Components/Table/Table.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Table.css'
+
+const Table = () => {
+	return (
+		<div className='table'>
+				<p className='grey'>CRYPTOCURRENCY</p>
+				<p className='grey'>PRICE</p>
+				<p className='grey'>MARKET CAP</p>
+				<p className='grey'>24H CHANGE</p>
+		</div>
+	)
+}
+
+export default Table;
